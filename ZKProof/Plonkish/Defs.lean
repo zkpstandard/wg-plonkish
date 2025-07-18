9e42bb445e7086af95599fdec6e8b49c5771035d
Daira-Emma Hopwood <daira@jacaranda.org>
2025-07-18 18:29:00 +0100

    Update Mathlib and fix breakage due to the change in definition of `Rel`.
    Signed-off-by: Daira-Emma Hopwood <daira@jacaranda.org>

--- ZKProof/Plonkish/Defs.lean
+++ ZKProof/Plonkish/Defs.lean
@@ -42,7 +42,7 @@ structure AbstractCircuit where
   /-- The number of columns that are fixed. -/
   m_f : Fin (m+1)
   /-- The fixed content of the first `m_f` columns. -/
-  f (e : Location m n) (is_fixed_col : e.i < m_f) : F
+  f (e : Location m n) (is_fixed_col : Fin.castSucc e.i < m_f) : F
 
   /-- The number of custom gates. The default is 0, in which case `p` and `CUS` need not be provided. -/
   U : ℕ := 0
@@ -90,9 +90,9 @@ The relation for a Plonkish abstract circuit with instance vector `φ`.
 We define this as a structure rather than a monolithic `Prop`, to more easily allow
 referring to its parts. Lean's type system still allows it to be used as a `Prop`.
 -/
-structure R (φ : C.Instance) (w : C.Witness) where
+structure R_parts (φ : C.Instance) (w : C.Witness) where
   /-- Semantics of fixed constraints. -/
-  fixed (e : C.Entry) (is_fixed_col : e.i < C.m_f) : w e = C.f e is_fixed_col
+  fixed (e : C.Entry) (is_fixed_col : Fin.castSucc e.i < C.m_f) : w e = C.f e is_fixed_col
 
   /-- Semantics of copy constraints for instance entries. -/
   input (k : Fin C.t) : w C.S[k] = φ[k]
@@ -104,6 +104,8 @@ structure R (φ : C.Instance) (w : C.Witness) where
 
   /-- Semantics of lookup constraints. -/
   lookup (v : Fin C.V) (j : C.LOOK v) : (C.q v).map (C.row_eval F w j) ∈ C.TAB v := by intro v; exact Fin.elim0 v
+
+def R : C.Relation := { (φ, w) | C.R_parts F φ w }
 
 end AbstractCircuit
 
@@ -134,7 +136,7 @@ def dt : AbstractCircuit F := {
   E _ _ := true
   -- E is an equivalence relation.
   Equivalence_E := {
-    refl := by intro x; simp only [gt_iff_lt]
+    refl := by intro x; simp only
     symm := by intro x; simp only [imp_self, implies_true]
     trans := by intro x; simp only [imp_self, implies_true]
   }
@@ -158,7 +160,7 @@ Completeness of the `dt` example circuit.
 Here we show completeness directly by constructing the only valid witness
 and showing that it is satisfying for the only valid instance vector.
 -/
-theorem dt_complete_direct : (dt F).R F #v[42] (dt_valid_witness F) := {
+theorem dt_complete_direct : (#v[42], dt_valid_witness F) ∈ (dt F).R F := {
   fixed e := by simp [dt, dt_valid_witness, dt_witness]
   input k := by simp; rfl
   equal e e' := by simp [dt, dt_valid_witness, dt_witness]
@@ -170,13 +172,13 @@ Soundness of the `dt` example circuit.
 We show that any satisfying witness implies `x = 42` (and also `a = c = 42`).
 -/
 theorem dt_knowledge_sound_direct (x c a : F)
-    (stmt : (dt F).R F #v[x] (dt_witness F c a))
+    (stmt : (#v[x], dt_witness F c a) ∈ (dt F).R F)
     : x = 42 ∧ a = 42 ∧ c = 42 :=
 by
-  obtain ⟨ fixed, input, equal ⟩ := stmt; simp at fixed
-  have hx_a : x = a  := by let hx := input (0 : Fin 1); subst hx; rfl
+  obtain ⟨ fixed, input, equal ⟩ := stmt
+  have hx_a : x = a  := by let hx := input (0 : Fin 1); exact hx.symm
   have ha_c : a = c  := by let ha_c := equal dt_a dt_c; simp [dt, dt_witness, dt_a, dt_c, dt_entry, entry] at ha_c; exact ha_c
-  have hc   : c = 42 := by let hc := fixed dt_c <| show dt_c.i < (dt F).m_f by simp [dt, dt_c, dt_entry, entry];
+  have hc   : c = 42 := by let hc := fixed dt_c <| show Fin.castSucc dt_c.i < (dt F).m_f by simp [dt, dt_c, dt_entry, entry];
                            simp [dt_c, dt_witness, dt_entry, entry] at hc; exact hc
   simp_all
 
@@ -184,8 +186,7 @@ by
 An alternative is to show that a refinement from an abstract relation is complete and sound.
 The abstract relation is very simple indeed.
 -/
-def abstract_dt (x : F) (_ : Unit) :=
-  x = 42
+def abstract_dt := { (x, _) : F × Unit | x = 42 }
 
 def dt_refinement : Refinement (abstract_dt F) (dt F).R where
   trans := {
@@ -204,7 +205,8 @@ def dt_complete_by_refinement : Complete (dt_refinement F) :=
     satisfied := {
       fixed e := by simp [dt, dt_valid_witness, dt_witness]
       input k := by rw [htrans, show k = (0 : Fin 1) by ext; simp];
-                    simp [hsat, dt, dt_valid_witness, dt_witness]
+                    simp [dt, dt_valid_witness, dt_witness]
+                    exact hsat.symm
       equal e e' := by simp [dt, dt_valid_witness, dt_witness]
     }
   }
@@ -217,10 +219,10 @@ def dt_knowledge_sound_by_refinement : KnowledgeSound (dt_refinement F) :=
       let a := sat'.w dt_a
       let c := sat'.w dt_c
       obtain ⟨ fixed, input, equal ⟩ := sat'.satisfied
-      let hc := fixed dt_c <| show dt_c.i < (dt F).m_f by simp [dt, dt_c, dt_entry, entry];
+      let hc := fixed dt_c <| show Fin.castSucc dt_c.i < (dt F).m_f by simp [dt, dt_c, dt_entry, entry];
       have hx_a : x = a := by let hx := input (0 : Fin 1); simp [dt, dt_a, dt_entry, entry] at hx; simp [a]; exact hx.symm
-      have ha_c : a = c := by let ha_c := equal dt_a dt_c; simp [dt, dt_witness, dt_a, dt_c, dt_entry, entry] at ha_c; exact ha_c
-      have c_42 : c = 42 := by simp [dt_witness, dt_c, dt_entry, entry] at hc; exact hc
+      have ha_c : a = c := by let ha_c := equal dt_a dt_c; simp [dt, dt_a, dt_c, dt_entry, entry] at ha_c; exact ha_c
+      have c_42 : c = 42 := by simp [dt_c, dt_entry, entry] at hc; exact hc
       simp_all
       exact hx_a
   }
@@ -229,7 +231,7 @@ end Example
 
 /-- Not sure whether this kind of lemma is useful yet. -/
 lemma check_constant (C : AbstractCircuit F) (x' : C.Instance) (sat' : Satisfying C.R x')
-    (e : Location C.m C.n) (is_fixed_col : e.i < C.m_f)
+    (e : Location C.m C.n) (is_fixed_col : Fin.castSucc e.i < C.m_f)
     : sat'.w e = C.f e is_fixed_col := by
   obtain ⟨ fixed ⟩ := sat'.satisfied
   rw [fixed e is_fixed_col]
